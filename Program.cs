b984d6adbdadb5112432e71a386cd0ba899da7c1
Mjerchel12 <122129884+Mjerchel12@users.noreply.github.com>
2023-03-12 21:49:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var bot = new Bot();
+        bot.RunAsync().GetAwaiter().GetResult();
+    }
+}
